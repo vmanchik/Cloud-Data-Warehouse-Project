2ce51b6a4c97960d9de1825283d8326897a07f17
Victor Manchik <vmanchik@fullcoll.edu>
2024-12-05 19:56:45 -0800

    Update create_tables.py
    clarified the language in the documentation strings

--- create_tables.py
+++ create_tables.py
@@ -5,7 +5,7 @@ from sql_queries import create_table_queries, drop_table_queries
 
 def drop_tables(cur, conn):
     """
-    Delete pre-existing tables to be able to create them from scratch
+    Delete pre-existing tables to allow recreation.
     """
     for query in drop_table_queries:
         cur.execute(query)
@@ -14,7 +14,7 @@ def drop_tables(cur, conn):
 
 def create_tables(cur, conn):
     """
-    Create staging and dimensional tables declared on sql_queries script
+    Create staging and dimensional tables as declared in the sql_queries script.
     """
     for query in create_table_queries:
         cur.execute(query)
@@ -23,7 +23,7 @@ def create_tables(cur, conn):
 
 def main():
     """
-    Set up the database tables, create needed tables with the appropriate columns and constricts
+    Set up the database tables by creating the necessary tables.
     """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
