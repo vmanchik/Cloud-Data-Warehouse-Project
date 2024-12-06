d20ebb197880bd215609ad6435c17b84e227817f
Victor Manchik <vmanchik@fullcoll.edu>
2024-12-05 19:32:20 -0800

    Update etl.py
    Included doc strings

--- etl.py
+++ etl.py
@@ -4,12 +4,18 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Populate the staging events and staging songs tables with data using copy method from the S3 storage
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Transform and insert data from the staging tables to the dimension tables and the fact table using sql queries
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
